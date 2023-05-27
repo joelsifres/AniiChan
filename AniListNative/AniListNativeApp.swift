b0f7ba5570f5133637ab1ac752f1bae0a57886f6
Joel Sifres <joelsifresclemente@gmail.com>
2023-05-27 07:33:21 +0200

    Initial Commit

---
+++ AniListNative/AniListNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniListNativeApp.swift
+//  AniListNative
+//
+//  Created by Joel Sifres on 27/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AniListNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
